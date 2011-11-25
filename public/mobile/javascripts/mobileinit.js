c0164dd6d35b65d7441bcc53aec747bca903f3a9
Adam Williams <adam@thewilliams.ws>
2011-11-25 15:31:50 -0500

    Removed old generated resources.

--- public/mobile/javascripts/mobileinit.js
+++
@@ -1,6 +0,0 @@
-
-  $(document).bind('mobileinit', function() {
-    $.mobile.ajaxEnabled = false;
-    $.mobile.hashListeningEnabled = false;
-    return $.mobile.linkBindingEnabled = false;
-  });
